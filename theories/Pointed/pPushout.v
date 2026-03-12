789212abba8ec02c1af08ccb170aa10c45a04dcf
PHart3 <perry.hart7@yahoo.com>
2026-03-11 19:27:51 -0500

    add two pushout maps

--- theories/Pointed/pPushout.v
+++ theories/Pointed/pPushout.v
@@ -1,20 +1,21 @@
-From HoTT Require Import Basics.
-Require Import Pointed.Core.
+From HoTT Require Import Basics Types.
+Require Import Pointed.Core pSusp.
 Require Import Colimits.Pushout.
+Require Import Homotopy.Wedge.
 
 Local Open Scope pointed_scope.
 
 (** * Pointed pushouts *)
 
-Definition pPushout {A B C : Type} `{ptB : IsPointed B} (f : A -> B) (g : A -> C) : pType
-  := [Pushout f g, pushl ptB].
+Definition pPushout {A C : Type} {B : pType} (f : A -> B) (g : A -> C) : pType
+  := [Pushout f g, pushl pt].
 
 (** *** Action on maps of spans *)
 
 Definition functor_pPushout
   {A C} {B : pType} {f : A -> B} {g : A -> C}
   {A' C'} {B' : pType} {f' : A' -> B'} {g' : A' -> C'}
-  (h : A -> A') (k : B ->* B') (l : C -> C')
+  (k : B ->* B') (h : A -> A') (l : C -> C')
   (p : k o f == f' o h) (q : l o g == g' o h) :
   pPushout f g ->* pPushout f' g'.
 Proof.
@@ -38,3 +39,22 @@ Section PtdLegsPO.
     := Build_pMap pushr ((ap pushr (point_eq g))^ @ (pglue pt)^ @ ap pushl (point_eq f)).
   
 End PtdLegsPO.
+
+(** *** Some useful span maps. *)
+
+Section SpanTransform.
+  
+  Context {X Y Z : pType} {f : Z ->* X} {g : Z ->* Y}.
+
+  Definition reglue : X \/ Y ->* pPushout f g
+    := functor_pPushout pmap_idmap (fun _ => pt) pmap_idmap
+         (fun _ => (point_eq f)^) (fun _ => (point_eq g)^) .
+
+  Definition ext_glue : pPushout f g ->* psusp Z.
+  Proof.
+    apply (functor_pPushout (B' := pUnit) pconst idmap (const_tt Y)).
+    - intros. reflexivity.
+    - intros. reflexivity.
+  Defined.
+  
+End SpanTransform.
