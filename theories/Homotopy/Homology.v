cb5a9a4f5ec8b4c58ab207f19089357d947bf31b
PHart3 <perry.hart7@yahoo.com>
2026-03-11 23:16:45 -0500

    add MV maps

--- theories/Homotopy/Homology.v
+++ theories/Homotopy/Homology.v
@@ -11,12 +11,13 @@ Local Open Scope pointed_scope.
 Generalizable Variables Z.
 
 Record HomologyTheory `{Integers Z} := {
-    C : Z -> pType -> AbGroup;
-    C_0ftor : forall z, Is0Functor (C z);
-    C_1ftor : forall z, Is1Functor (C z);
-    C_exactness : forall {z : Z} {X Y : pType} (f : X ->* Y),
-      GrpIsExact (fmap (C z) f) (fmap (C z) (ptd_cofib f));
-    C_susp : forall {z : Z} (X : pType), GroupIsomorphism (C (1 + z) (psusp X)) (C z X);
-    C_susp_fmap : forall {z : Z} {X Y : pType} (f : X ->* Y) (x : C (1 + z) (psusp X)),
-      fmap (C z) f ∘ C_susp X == C_susp Y ∘ fmap (C (1 + z)) (fmap psusp f);
+    C_obj : Z -> pType -> AbGroup;
+    C_0ftor :: forall z, Is0Functor (C_obj z);
+    C_1ftor : forall z, Is1Functor (C_obj z);
+    C_exactness : forall {n : Z} {X Y : pType} (f : X ->* Y),
+      GrpIsExact (fmap (C_obj n) f) (fmap (C_obj n) (ptd_cofib f));
+    C_susp : forall {n : Z} (X : pType),
+      GroupIsomorphism (C_obj (1 + n) (psusp X)) (C_obj n X);
+    C_susp_fmap : forall {n : Z} {X Y : pType} (f : X ->* Y) (x : C_obj (1 + n) (psusp X)),
+      fmap (C_obj n) f ∘ C_susp X == C_susp Y ∘ fmap (C_obj (1 + n)) (fmap psusp f);
     }.
