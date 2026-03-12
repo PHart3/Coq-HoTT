8fc5e22a7f105be88e11c6e928a484fffa35169e
PHart3 <perry.hart7@yahoo.com>
2026-03-11 23:16:45 -0500

    add MV maps

---
+++ theories/Homotopy/MayerVietorisHomol.v
@@ -0,0 +1,30 @@
+From HoTT Require Import Basics Classes.interfaces.integers abstract_algebra.
+From HoTT.WildCat Require Import Core Universe.
+Require Import Groups.Group ExactSeq.
+Require Import AbGroups.AbelianGroup AbHom Biproduct.
+Require Import Pointed.Core pPushout pSusp pCofiber.
+Require Import Homotopy.Homology.
+
+Local Open Scope pointed_scope.
+
+(** * The Mayer-Vietoris sequence for homology *)
+
+Generalizable Variables Z.
+
+Section MVHom.
+
+  Context `{Integers Z} (Hom : HomologyTheory)
+    {C A B : pType} (f : C ->* A) (g : C ->* B) {n : Z}.
+
+  Definition MV_boundary_map : C_obj Hom (1 + n) (ppushout f g) $-> C_obj Hom n C
+    := C_susp Hom C $o fmap (C_obj Hom (1 + n)) ext_glue.
+
+  Definition MV_push_diff :
+    ab_biprod (C_obj Hom n A) (C_obj Hom n B) $-> C_obj Hom n (ppushout f g)
+    := ab_biprod_rec
+         (fmap (C_obj Hom n) ptd_pushl) (inverse_hom (fmap (C_obj Hom n) ptd_pushr)).
+
+  Definition MV_po_to_prod : C_obj Hom n C $-> ab_biprod (C_obj Hom n A) (C_obj Hom n B)
+    := ab_biprod_corec (fmap (C_obj Hom n) f) (fmap (C_obj Hom n) g).
+  
+End MVHom.
--- theories/Pointed/pPushout.v
+++ theories/Pointed/pPushout.v
@@ -7,17 +7,17 @@ Local Open Scope pointed_scope.
 
 (** * Pointed pushouts *)
 
-Definition pPushout {A C : Type} {B : pType} (f : A -> B) (g : A -> C) : pType
+Definition ppushout {A C : Type} {B : pType} (f : A -> B) (g : A -> C) : pType
   := [Pushout f g, pushl pt].
 
 (** *** Action on maps of spans *)
 
-Definition functor_pPushout
+Definition functor_ppushout
   {A C} {B : pType} {f : A -> B} {g : A -> C}
   {A' C'} {B' : pType} {f' : A' -> B'} {g' : A' -> C'}
   (k : B ->* B') (h : A -> A') (l : C -> C')
   (p : k o f == f' o h) (q : l o g == g' o h) :
-  pPushout f g ->* pPushout f' g'.
+  ppushout f g ->* ppushout f' g'.
 Proof.
   snapply Build_pMap.
   - napply (functor_pushout h k l p q).
@@ -32,10 +32,10 @@ Section PtdLegsPO.
 
   Context {A B C : pType} {f : A ->* B} {g : A ->* C}.
 
-  Definition ptd_pushl : B ->* pPushout f g
+  Definition ptd_pushl : B ->* ppushout f g
     := Build_pMap pushl 1.
 
-  Definition ptd_pushr : C ->* pPushout f g
+  Definition ptd_pushr : C ->* ppushout f g
     := Build_pMap pushr ((ap pushr (point_eq g))^ @ (pglue pt)^ @ ap pushl (point_eq f)).
   
 End PtdLegsPO.
@@ -46,13 +46,13 @@ Section SpanTransform.
   
   Context {X Y Z : pType} {f : Z ->* X} {g : Z ->* Y}.
 
-  Definition reglue : X \/ Y ->* pPushout f g
-    := functor_pPushout pmap_idmap (fun _ => pt) pmap_idmap
+  Definition reglue : X \/ Y ->* ppushout f g
+    := functor_ppushout pmap_idmap (fun _ => pt) pmap_idmap
          (fun _ => (point_eq f)^) (fun _ => (point_eq g)^) .
 
-  Definition ext_glue : pPushout f g ->* psusp Z.
+  Definition ext_glue : ppushout f g ->* psusp Z.
   Proof.
-    apply (functor_pPushout (B' := pUnit) pconst idmap (const_tt Y)).
+    apply (functor_ppushout (B' := pUnit) pconst idmap (const_tt Y)).
     - intros. reflexivity.
     - intros. reflexivity.
   Defined.
