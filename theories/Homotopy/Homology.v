c6f4de849ee6e80b38116ea301146601d6b9fb07
Cal W <calwojahn@gmail.com>
2026-04-30 14:13:40 -0500

    Made path algebra more idiomatic; Began topological sublemma.

--- theories/Homotopy/Homology.v
+++ theories/Homotopy/Homology.v
@@ -2,8 +2,10 @@ From HoTT Require Import Basics abstract_algebra.
 From HoTT.WildCat Require Import Core Universe.
 Require Import Groups.Group ExactSeq.
 Require Import AbGroups.AbelianGroup.
-Require Import Pointed.Core pSusp pCofiber.
 Require Import SuccessorStructure.
+Require Import Colimits.Pushout.
+Require Import Pointed.Core pCofiber pEquiv pSusp.
+Require Import Homotopy.Cofiber Wedge.
 
 Local Open Scope succ_scope.
 Local Open Scope pointed_scope.
@@ -81,44 +83,95 @@ Section Homol_bin_wedge.
     - apply BuildIsSurjection.
       intro h1h2; destruct h1h2 as [h1 h2].
       apply tr.
-      cbn.
       exists ((i1 h1) * (i2 h2)).
       apply (path_prod').
-      + set (p := im_sub_ker _ _ i2_j1_isexact (i2 h2) (tr (n:=-1) (h2; idpath (i2 h2)))).
-        cbn in p.       
-        set (p1 := ap ((j1 (i1 h1))*.) p).
-        set (p2 := (grp_homo_op j1 (i1 h1) (i2 h2)) @ p1 @ grp_unit_r (j1 (i1 h1))).
-        exact (concat_r (htpy1 h1) p2).
-        
-      + set (q := im_sub_ker _ _ i1_j2_isexact (i1 h1) (tr (n:=-1) (h1; idpath (i1 h1)))).   
-        set (q1 := ap (.*(j2 (i2 h2))) q).
-        set (q2 := grp_homo_op j2 (i1 h1) (i2 h2) @ q1 @ grp_unit_l (j2 (i2 h2))).
-        exact (concat_r (htpy2 h2) q2).  
+      + lhs apply (grp_homo_op j1 (i1 h1) (i2 h2)).
+        lhs apply (ap (.*(j1 (i2 h2))) (htpy1 h1)).
+        rhs_V apply (grp_unit_r h1).
+        apply (grp_cancelL h1).
+        exact (im_sub_ker _ _ i2_j1_isexact (i2 h2) (tr (h2; idpath (i2 h2)))).
+      + lhs apply (grp_homo_op j2 (i1 h1) (i2 h2)).
+        lhs apply (ap ((j2 (i1 h1))*.) (htpy2 h2)).
+        rhs_V apply (grp_unit_l h2).
+        apply (grp_cancelR h2).
+        exact (im_sub_ker _ _ i1_j2_isexact (i1 h1) (tr (h1; idpath (i1 h1)))).
     - intro y; destruct y as [y1 y2].
       apply isembedding_istrivial_kernel.
-      intro g.
-      cbn.
-      intro pair_path.
-      set (p1 := (ap fst pair_path)); cbn in p1.
-      set (p2 := (ap snd pair_path)); cbn in p2.
-      set (im_witness := ker_sub_im _ _ i1_j2_isexact g p2).
-      cbn in im_witness.
-      rapply (Trunc_rec (n:=-1) _ im_witness).
-      + napply sig_rec.
-        intros h1 wpath.
-        rhs_V apply (grp_homo_unit i1).
-        rhs_V apply (ap i1 p1).
-        rhs_V apply (ap (i1 $o j1) wpath).
-        rhs apply (ap i1 (htpy1 h1)).
-        rhs apply wpath.
-        reflexivity.
+      intros g pair_path.
+      pose (p2 := (ap snd pair_path)).
+      pose (im_witness := ker_sub_im _ _ i1_j2_isexact g p2).
+      rapply (Trunc_rec _ im_witness).
+      napply sig_rec.
+      intros h1 wpath.
+      rhs_V apply (grp_homo_unit i1).
+      rhs_V apply (ap (i1 o fst) pair_path).
+      rhs_V apply (ap (i1 $o j1) wpath).
+      rhs apply (ap i1 (htpy1 h1)).
+      symmetry; apply wpath.
   Qed.
 
-  (*
-  Lemma top_th_lemma
-    {X Y : pType}
-    : (pcofiber wedge_inl) <~> Y.
-    
-  Qed.
+(*  Definition grp_prod_iso
+    {G H1 H2 : Group}
+    {i1 : H1 $-> G}
+    {i2 : H2 $-> G}
+    {j1 : G $-> H1}
+    {j2 : G $-> H2}
+    (htpy1 : j1 o i1 == idmap)
+    (htpy2 : j2 o i2 == idmap)
+    (i1_j2_isexact : GrpIsExact i1 j2)
+    (i2_j1_isexact : GrpIsExact i2 j1)
+    : GroupIsomorphism G (grp_prod H1 H2) := Build_GroupIsomorphism G (grp_prod H1 H2) (grp_prod_corec j1 j1) _.
+  Proof
+    apply grp_th_lemma.
+  Defined.
   *)
+
+  Definition cof_to_Y {X Y : pType} : pcofiber (@wedge_inl X Y) -> Y.
+  Proof.
+    snapply Pushout_rec.
+    - exact wedge_pr2.
+    - intro pnt; exact (point Y).
+    - intro x; reflexivity.    
+  Defined.
+
+  Definition Y_to_cof {X Y : pType} : Y -> pcofiber (@wedge_inl X Y).
+  Proof.
+    intro y.
+    apply (cofib wedge_inl).
+    apply (wedge_inr y).
+  Defined.
+
+(*  Definition cof_to_Y_to_cof
+    {X Y : pType}
+    {f : pcofiber (@wedge_inl X Y) -> Y}
+    {g : Y -> pcofiber (@wedge_inl X Y)}
+    : g o f == idmap.
+  Proof.
+    snapply cofiber_ind.
+    - cbn.
+      snapply wedge_ind.
+      + intro x.
+        cbn.
+        
+        pose (mycfglue := cfglue (@wedge_inl X Y) x). 
+        pose (mywglue := ap (cofib wedge_inl) (@wglue X Y)).
+        rhs apply mycfglue.
+
+        
+  Definition cof_wedge_inl_cod_equiv {X Y : pType} : pcofiber (@wedge_inl X Y) <~> Y.
+  Proof.
+    snapply Build_Equiv.
+     (* pose (f := Pushout_rec Y wedge_pr2). *)
+    - exact f.
+    - snapply Build_IsEquiv.
+      + exact g.
+      + reflexivity.
+      +
+  Defined.
+
+  Definition cof_wedge_inl_cod_pequiv := term.
+  Proof.
+    snapply Build_pEquiv'.
+  Defined
+  *)       
 End Homol_bin_wedge.
