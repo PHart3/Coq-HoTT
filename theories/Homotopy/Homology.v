a1cb9c7c0d26c02c0026195edfed305efb0c395e
PHart3 <perry.hart7@yahoo.com>
2026-06-20 11:34:17 -0500

    define long MV sequence

--- theories/Homotopy/Homology.v
+++ theories/Homotopy/Homology.v
@@ -1,44 +1,43 @@
-From HoTT Require Import Basics Classes.interfaces.integers abstract_algebra.
+From HoTT Require Import Basics abstract_algebra.
 From HoTT.WildCat Require Import Core Universe.
 Require Import Groups.Group ExactSeq.
 Require Import AbGroups.AbelianGroup.
 Require Import Pointed.Core pSusp pCofiber.
+Require Import SuccessorStructure.
 
+Local Open Scope succ_scope.
 Local Open Scope pointed_scope.
 
 (** * Generalized homology theories *)
 
-Generalizable Variables Z.
-
-Class HomologyTheory `{Integers Z} := {
-    C_obj : Z -> pType -> AbGroup;
+Class HomologyTheory `{N : SuccStr} := {
+    C_obj : N -> pType -> AbGroup;
     C_0ftor :: forall z, Is0Functor (C_obj z);
     C_1ftor :: forall z, Is1Functor (C_obj z);
-    C_exactness : forall {n : Z} {X Y : pType} (f : X ->* Y),
+    C_exactness : forall {n : N} {X Y : pType} (f : X ->* Y),
       GrpIsExact (fmap (C_obj n) f) (fmap (C_obj n) (ptd_cofib f));
-    C_susp : forall {n : Z} (X : pType),
-      GroupIsomorphism (C_obj (1 + n) (psusp X)) (C_obj n X);
-    C_susp_fmap : forall {n : Z} {X Y : pType} (f : X ->* Y),
-      fmap (C_obj n) f ∘ C_susp X == C_susp Y ∘ fmap (C_obj (1 + n)) (fmap psusp f);
+    C_susp : forall {n : N} (X : pType),
+      GroupIsomorphism (C_obj (n .+1) (psusp X)) (C_obj n X);
+    C_susp_fmap : forall {n : N} {X Y : pType} (f : X ->* Y),
+      fmap (C_obj n) f ∘ C_susp X == C_susp Y ∘ fmap (C_obj (n .+1)) (fmap psusp f);
     }.
 
 (** Homology is independent of basepoint *)
 
 Section Basepoint_ind.
   
-  Context `{Hom : HomologyTheory} {n : Z}.
+  Context `{Hom : HomologyTheory} {n : N}.
   
   Definition Homol_obj_bpind {X : Type} {x y : X} : GroupIsomorphism (C_obj n [X, x]) (C_obj n [X, y]) :=
     grp_iso_compose (C_susp [X, y]) (grp_iso_inverse (C_susp [X, x])).
 
   Lemma C_susp_fmap_rotate {X Y : pType} {f : X ->* Y} :
-    fmap (C_obj n) f == C_susp Y ∘ fmap (C_obj (1 + n)) (fmap psusp f) ∘ grp_iso_inverse (C_susp X).
+    fmap (C_obj n) f == C_susp Y ∘ fmap (C_obj (n .+1)) (fmap psusp f) ∘ grp_iso_inverse (C_susp X).
   Proof.
     intro x.
     refine (_@ (C_susp_fmap f (grp_iso_inverse (C_susp X) x))). refine (ap (fmap (C_obj n) f) _).
     symmetry. apply eisretr.
   Qed.
-  
   
   Lemma Homol_mor_bpind {X Y : pType} (f : X -> Y) {p q : f pt = pt} :
     fmap (C_obj n) (Build_pMap f p) == fmap (C_obj n) (Build_pMap f q).
--- theories/Homotopy/MV_Homology/MayerVietorisHomol.v
+++ theories/Homotopy/MV_Homology/MayerVietorisHomol.v
@@ -1,25 +1,22 @@
-From HoTT Require Import Basics Classes.interfaces.integers abstract_algebra.
+From HoTT Require Import Basics abstract_algebra.
 From HoTT.WildCat Require Import Core Universe.
 Require Import Groups.Group ExactSeq.
 Require Import AbGroups.AbelianGroup AbHom Biproduct.
 Require Import Pointed.Core pPushout pSusp pCofiber.
-Require Import Homotopy.Homology.
+Require Import Homotopy.Homology SuccessorStructure.
 
 Local Open Scope pointed_scope.
+Local Open Scope succ_scope.
 
 (** * The Mayer-Vietoris sequence for homology *)
 
-Generalizable Variables Z.
+Generalizable Variables N.
 
-Section MVHom.
+Section MVMaps.
 
-  Context `{Hom : HomologyTheory} {C A B : pType} {n : Z} `{Funext}.
+  Context `{HomologyTheory} {C A B : pType} {n : N}.
 
   (** We begin with the three maps that generate the long sequence. *)
-  
-  Definition MV_boundary_map : forall {f : C ->* A} {g : C ->* B},
-      C_obj (1 + n) (ppushout f g) $-> C_obj n C
-    := fun f g => C_susp C $o fmap (C_obj (1 + n)) (ext_glue f g).
 
   Definition MV_push_diff : forall {f : C ->* A} {g : C ->* B},
     ab_biprod (C_obj n A) (C_obj n B) $-> C_obj n (ppushout f g)
@@ -29,6 +26,10 @@ Section MVHom.
   Definition MV_po_to_prod : forall (f : C ->* A) (g : C ->* B),
       C_obj n C $-> ab_biprod (C_obj n A) (C_obj n B)
     := fun f g => ab_biprod_corec (fmap (C_obj n) f) (inverse_hom (fmap (C_obj n) g)).
+
+  Definition MV_boundary_map : forall {f : C ->* A} {g : C ->* B},
+      C_obj (n .+1) (ppushout f g) $-> C_obj n C
+    := fun f g => C_susp C $o fmap (C_obj (n .+1)) (ext_glue f g).
 
   (** In homology, ext_glue fits into a commuting triangle with the cofiber-suspension equivalence. *)
   Lemma MV_boundary_cof_susp_equiv {f : C ->* A} {g : C ->* B} :
@@ -40,5 +41,28 @@ Section MVHom.
     + exact (Homol_mor_bpind (ext_glue f g)).
   Qed.
 
-End MVHom.
+End MVMaps.
 
+(** The full Mayer-Vietoris long sequence *)
+
+Section MVSeq.
+  
+  Context `{HomologyTheory (N := +Z)} {C A B : pType} (f : C ->* A) (g : C ->* B).
+
+  Definition MV_long_seq_carrier (n : Z3) : AbGroup :=
+    match n with
+    | (n, inl (inl (inl x))) => Empty_ind _ x
+    | (n, inl (inl (inr tt))) => C_obj n (ppushout f g) 
+    | (n, inl (inr tt)) => ab_biprod (C_obj n A) (C_obj n B)
+    | (n, inr tt) => C_obj n C
+    end.
+  
+  Definition MV_long_seq : forall n, MV_long_seq_carrier (n .+1) ->* MV_long_seq_carrier n.
+  Proof.
+    intros [n [[[[]|[]]|[]]|[]]]; cbn.
+    - exact MV_push_diff.
+    - exact (MV_po_to_prod f g).
+    - exact MV_boundary_map.
+  Defined.
+  
+End MVSeq.
